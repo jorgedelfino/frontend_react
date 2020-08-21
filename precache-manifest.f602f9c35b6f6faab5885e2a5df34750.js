780d6c33e571a31a13fd1129a324a5929bb32ec2
Jorge Delfino <delfinojorge94@gmail.com>
2020-08-21 19:45:15 -0300

    primer commit

---
+++ precache-manifest.f602f9c35b6f6faab5885e2a5df34750.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "340b416c9cff6c6ed843749318b2d494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57899352b8eba9afe71d",
+    "url": "/static/css/main.5e83d98a.chunk.css"
+  },
+  {
+    "revision": "9ae5f80f2ec0df57d3cf",
+    "url": "/static/js/2.91e320d3.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.91e320d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57899352b8eba9afe71d",
+    "url": "/static/js/main.bf86474b.chunk.js"
+  },
+  {
+    "revision": "7154beb2b8c77f151aa5",
+    "url": "/static/js/runtime-main.ac284b60.js"
+  }
+]);
